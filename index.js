a236fcb28e025879d9442da7b82c85b9f46e8bc4
Jenkins <node-api-sdk-support@rtsmunity.com>
2016-11-08 13:00:20 +0000

    Update

--- index.js
+++ index.js
@@ -2,44 +2,16 @@
 
 var util = require('util')
 var fs = require('fs')
-var sdk = require('./src/index.js')
-
-var API_URL = {
-  local: 'https://localhost:3000',
-  production: 'https://api.rtsmunity.com:6220',
-  sandbox: 'https://api.sandbox.rtsmunity.com:6210'
-}
-
-// Extended API client
-function ApiClient (apiKey, clientKeyPath, clientCertPath, passphrase, mode) {
-  sdk.ApiClient.call(this)
-
-  this.basePath = API_URL[mode] || API_URL['local']
-  this.defaultHeaders = {
-    'X-Api-Key': apiKey
-  }
-  this._key = fs.readFileSync(clientKeyPath, 'utf-8')
-  this._cert = fs.readFileSync(clientCertPath, 'utf-8')
-  this._passphrase = passphrase
-}
-
-ApiClient.prototype.applyAuthToRequest = function (request, authNames) {
-  ApiClient.super_.prototype.applyAuthToRequest.call(this, request, authNames)
-  request.key(this._key)
-  request.cert(this._cert)
-  request.passphrase(this._passphrase)
-}
-
-util.inherits(ApiClient, sdk.ApiClient)
+var sdk = require('./src/index')
+var MODE = require('./src/mode')
 
 function toLowerCase (word) {
   if (word.length === 0) { return word }
   return word.charAt(0).toLowerCase() + word.substr(1)
 }
 
-// Export patched classes
-module.exports = function (apiKey, clientKeyPath, clientCertPath, passphrase, mode) {
-  var apiClient = new ApiClient(apiKey, clientKeyPath, clientCertPath, passphrase, mode)
+function init (apiKey, clientKeyPath, clientCertPath, passphrase, mode) {
+  var apiClient = new sdk.ApiClient(apiKey, clientKeyPath, clientCertPath, passphrase, mode)
 
   var resultSdk = {}
   for (var i in sdk) {
@@ -53,4 +25,10 @@ module.exports = function (apiKey, clientKeyPath, clientCertPath, passphrase, mo
   }
 
   return resultSdk
+}
+
+// Export patched classes
+module.exports = {
+  init: init,
+  MODE: MODE
 }
---
+++ src/mode.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PRODUCTION: 'production',
+  SANDBOX: 'sandbox',
+  LOCAL: 'local'
+}
